960495ed0ed9174c9e66b742b0225535dc001492
Gabriel <gabriel.wambugu@student.moringaschool.com>
2023-03-07 22:54:19 +0300

    Lab done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+greet "Jimmy"
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default "Naureen"
+greet_with_default
+
+def add num1, num2
+    num1 + num2
+end
+
+puts add 2,5
+
+def halve num
+    if num.class!= Integer
+        return nil
+    end
+    return num/2
+end
+
+puts halve 6
